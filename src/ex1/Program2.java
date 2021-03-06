437ccafa9efecfe2dc583577727eb109e3b43481
s117t <s117t@JUNGTAEPC>
2021-03-06 12:31:54 +0900

    Insert Program1

---
+++ src/ex1/Program2.java
@@ -0,0 +1,44 @@
+package ex1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+// INSERT Program
+public class Program2 {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		String title = "TEST2";
+		String writerId = "newlec";
+		String content = "hahaha";
+		String files = "";
+		
+		String url = "jdbc:mysql://localhost:3306/jspdb?serverTimezone=UTC";
+		String sql = "INSERT INTO `jspdb`.`notice`"
+				+ "("
+				+ "`TITLE`,"
+				+ "`WRITER_ID`,"
+				+ "`CONTENT`,"
+				+ "`FILES`)"
+				+ "VALUES (?,?,?,?)";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url,"root","3302");
+//		Statement st = con.createStatement();
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, title);
+		st.setString(2, writerId);
+		st.setString(3, content);
+		st.setString(4, files);
+		
+		int  result = st.executeUpdate();
+		
+		System.out.println(result);
+		
+		
+		st.close();
+		con.close();
+	}
+
+}
